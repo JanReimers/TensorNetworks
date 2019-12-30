b9e8ec21e0ec1b7c86527e6f1da44971161f51be
Jan Reimers <janreimers@gmail.com>
2019-12-30 18:49:12 +0000

    Add test for complex<double> SVD for square matrices.
    git-svn-id: https://subversion.assembla.com/svn/celldesigntools/TensorNetworks@1210 e8b80fd4-3b04-4a2c-9396-e76f703d3c4f

--- TensorNetworksUnittests/SVDTesting.cpp
+++ TensorNetworksUnittests/SVDTesting.cpp
@@ -2,6 +2,7 @@
 #include "TensorNetworks/MatrixProductState.H"
 #include "oml/stream.h"
 #include "oml/numeric.h"
+#include "oml/cnumeric.h"
 #include "oml/random.h"
 #include "oml/vector_io.h"
 #include "oml/array_io.h"
@@ -38,13 +39,14 @@ template <class Ob> std::string ToString(const Ob& result)
     return res_stream.str();
 }
 
-DMatrix<double> contract(const DMatrix<double>& U, const Vector<double>& s, const DMatrix<double>& V)
+template <class T,class T2>
+DMatrix<T2> contract(const DMatrix<T2> U, const Vector<T>& s, const DMatrix<T2>& V)
 {
     int ni=U.GetNumRows();
     int nk=U.GetNumCols();
     int nj=V.GetNumRows();
-    DMatrix<double> M(ni,nj);
-    Fill(M,0.0);
+    DMatrix<T2> M(ni,nj);
+    Fill(M,T2(0.0));
     for(int i=1;i<=ni;i++)
         for(int j=1;j<=nj;j++)
             for(int k=1;k<=nk;k++)
@@ -53,12 +55,14 @@ DMatrix<double> contract(const DMatrix<double>& U, const Vector<double>& s, cons
             }
     return M;
 }
-DMatrix<double> ConstractsV(const Vector<double>& s, const DMatrix<double>& V)
+
+template <class T,class T2>
+DMatrix<T2> ConstractsV(const Vector<T>& s, const DMatrix<T2>& V)
 {
     int ni=V.GetNumRows();
     int nk=s.GetHigh();
     int nj=V.GetNumRows();
-    DMatrix<double> Vs(ni,nj);
+    DMatrix<T2> Vs(ni,nj);
     for(int i=1;i<=ni;i++)
         for(int j=1;j<=nj;j++)
             for(int k=1;k<=nk;k++)
@@ -116,5 +120,73 @@ TEST_F(SVDTesting,OML_SVDRandomRectRealMatrix_5x10)
     EXPECT_NEAR(Max(fabs(VT*Transpose(VT)-UnitMatrix)),0.0,eps);
     EXPECT_NEAR(Max(fabs(M*ConstractsV(s,VT)-Mcopy)),0.0,eps);
 }
+
+
+
+//The eigen header file 'lib' is another option for later.  It takes a long time to complile.
+/*#include <Eigen/Dense>
+#include "oml/random.h"
+
+TEST_F(SVDTesting,OML_SVDRandomSquareComplexMatrix)
+{
+    using namespace Eigen;
+    int N=5;
+    MatrixXd m(N,N);
+    for (int i=0;i<N;i++)
+        for (int j=0;i<N;i++)
+            m(i,j)=OMLRandPos<double>();
+    BDCSVD< MatrixXd > svd(m);
+    std::cout << m << std::endl;
+}
+*/
+
+
+TEST_F(SVDTesting,SVDComplexSquare_N10)
+{
+    int N=10;
+    typedef DMatrix<eType> Mtype;
+    Mtype A(N,N),V(N,N),UnitMatrix(N,N);
+    Vector<double>  s(N);
+    FillRandom(A);
+    Mtype Mcopy(A);
+    Unit(UnitMatrix);
+    CSVDecomp(A,s,V);
+    EXPECT_NEAR(Max(abs(Transpose(conj(A))*A-UnitMatrix)),0.0,eps);
+    EXPECT_NEAR(Max(abs(V*Transpose(conj(V))-UnitMatrix)),0.0,eps);
+    Mtype Vstar=conj(V);
+    EXPECT_NEAR(Max(abs(A*ConstractsV(s,Vstar)-Mcopy)),0.0,eps);
+
+}
+
+/*
+TEST_F(SVDTesting,OML_SVDRandomSquareComplexMatrix)
+{
+    int N=4;
+    typedef DMatrix<std::complex<double> > Mtype;
+    Mtype M(N,N),VT(N,N),UnitMatrix(N,N);
+    Vector<std::complex<double> >  s(N);
+    FillRandom(M);
+    Mtype Mcopy(M+Transpose(conj(M)));
+    M=Mcopy;
+    Unit(UnitMatrix);
+//    cout << "UnitMatrix=" << UnitMatrix;
+//    cout << "M=" << M;
+    SVDecomp(M,s,VT);
+
+    cout << "s=" << s;
+    cout << "U=" << M;
+     cout << "Ut*U=" << Mtype(Transpose(conj(M))*M);
+     cout << "U*Ut=" << Mtype(M*Transpose(conj(M)));
+
+
+    //EXPECT_NEAR(Max(abs(Transpose(conj(M))*M-UnitMatrix)),0.0,eps);
+    //EXPECT_NEAR(Max(abs(M*Transpose(conj(M))-UnitMatrix)),0.0,eps);
+    //EXPECT_NEAR(Max(abs(Transpose(conj(VT))*VT-UnitMatrix)),0.0,eps);
+    //EXPECT_NEAR(Max(abs(VT*Transpose(conj(VT))-UnitMatrix)),0.0,eps);
+//    EXPECT_NEAR(Max(abs(M*ConstractsV(s,VT)-Mcopy)),0.0,eps);
+//    EXPECT_NEAR(Max(fabs(M*V-Mcopy)),0.0,eps);
+}
+*/
+
 
 
