9387b3607e59be6fddc8711a42ca7753322f3ac9
Jan Reimers <janreimers@gmail.com>
2020-07-17 18:45:37 +0000

    PC build: Try to use relative paths
    git-svn-id: https://subversion.assembla.com/svn/celldesigntools/TensorNetworks@1302 e8b80fd4-3b04-4a2c-9396-e76f703d3c4f

--- PyTensorNetworks/configure.py
+++ PyTensorNetworks/configure.py
@@ -13,26 +13,28 @@ config = sipconfig.Configuration()
 # Note: we are not picking C headers (*.h) YET, that comes below
 # The secret sauce seems to be the the -n wx.siplib without that the module won't load into python3
 #     I think this tells sip to use Robin's build of siblib (c files in wxPython-4.1/src) instead of building a local copy
-os.system(" ".join([config.sip_bin, "-c", ".", "-I/home/jan/wxPython-4.1/sip/gen/","-I/home/jan/wxPython-4.1/src","-I../../Plotting/PyPlotting", "-n wx.siplib","-b", build_file, "PyTensorNetworks.sip"]))
+os.system(" ".join([config.sip_bin, "-c", ".", "-I../../../wxPython-4/sip/gen/","-I../../../wxPython-4/src","-I../../Plotting/PyPlotting", "-n wx.siplib","-b", build_file, "PyTensorNetworks.sip"]))
 
 # Create the Makefile.
-makefile = sipconfig.SIPModuleMakefile(config, build_file,debug=0)
+makefile = sipconfig.SIPModuleMakefile(config, build_file,warnings=None,debug=1)
 
 #
 #  Here is where we tell the build where to find sip.h and any other headers in wxPython-4.1 source tree
 #
-makefile.extra_include_dirs=["/home/jan/wxPython-4.1/sip/siplib","../" ,"../../","../../Plotting","/usr/lib/x86_64-linux-gnu/wx/include/gtk3-unicode-3.0","/usr/include/wx-3.0"]
+makefile.extra_include_dirs=["../../../wxPython-4/sip/siplib","../" ,"../../","../../Plotting","/usr/lib/x86_64-linux-gnu/wx/include/gtk3-unicode-3.0","/usr/include/wx-3.0"]
 #
 #  Some compile flags required by wxWidgets
 #
-makefile.extra_defines=["HAVE_CONFIG_H","_FILE_OFFSET_BITS=64","WXUSINGDLL","__WXGTK__"]
+makefile.extra_defines=["_FILE_OFFSET_BITS=64","WXUSINGDLL","__WXGTK__"]
+makefile.extra_cxxflags=["-pthread"]
 #
 #  replace ../bin/Debug with a path to your extension lib
 #  /home/jan/.local/lib/python3.7/site-packages/wx is the location of the shared wx libs used by wxPython
 #  You will see that these are sent to the linker with -rpath, which should set the runtime path as well.
 #  This may be important because its easy to wxWidget .so's sprinkled in various places in a linux dist.
 #
-makefile.extra_lib_dirs=["../../Release","/home/jan/.local/lib/python3.7/site-packages/wx"]
+makefile.extra_lib_dirs=["../../Debug","/usr/local/lib/python3.7/dist-packages/wx"]
+makefile.extra_lflags=["-pthread"]
 # Add the library we are wrapping.  The name doesn't include any platform
 # specific prefixes or extensions (e.g. the "lib" prefix on UNIX, or the
 # ".dll" extension on Windows).
