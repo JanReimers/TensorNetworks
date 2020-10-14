99249cf1794d614e0e40405439b5dd3c6e127bf2
Jan Reimers <janreimers@gmail.com>
2020-10-14 09:21:03 -0500

    Move some function from MPO down to Operator
    This brings into question why we need both classes.  All MPOs stored the full lattice, possibly with lots of identity ops then everything would be much simpler.

--- TensorNetworks/MPO.C
+++ TensorNetworks/MPO.C
@@ -49,28 +49,6 @@ double MPO::Compress(const SVCompressorR* compressor)
     return percent;
 }
 
-int MPO::GetMaxDw() const
-{
-    int L=GetL();
-    int Dw=0;
-    for (int ia=1;ia<L;ia++)
-    {
-        Dw=Max(Dw,GetSiteOperator(ia)->GetDw12().Dw2);
-    }
-    return Dw;
-}
 
-
-void MPO::Report(std::ostream& os) const
-{
-    int L=GetL();
-    os << "Matrix Product Operator for " << L << " sites." << std::endl;
-    for (int ia=1;ia<=L;ia++)
-    {
-        os << "   Site " << ia << ": ";
-        GetSiteOperator(ia)->Report(os);
-        os << std::endl;
-    }
-}
 
 }
