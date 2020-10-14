5f59ee409be80bbf7816670672ef4abb243dbd52
Jan Reimers <janreimers@gmail.com>
2020-10-14 09:23:07 -0500

    Make a convenience NormalizeAndCompress(Direction LR,int Dmax,double eps); function

--- TensorNetworks/MPS.C
+++ TensorNetworks/MPS.C
@@ -1,4 +1,6 @@
 #include "TensorNetworks/MPS.H"
+#include "TensorNetworks/Factory.H"
+#include "TensorNetworks/SVCompressor.H"
 
 namespace TensorNetworks
 {
@@ -6,6 +8,13 @@ namespace TensorNetworks
 void MPS::Normalize(Direction lr)
 {
     NormalizeAndCompress(lr,0);
+}
+
+void MPS::NormalizeAndCompress(Direction lr,int Dmax,double epsSV)
+{
+    SVCompressorC* comp=Factory::GetFactory()->MakeMPSCompressor(Dmax,epsSV);
+    NormalizeAndCompress(lr,comp);
+    delete comp;
 }
 
 }
