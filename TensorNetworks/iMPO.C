e50ec7ada692a208bca0b973e9778b82f79e2c8e
Jan Reimers <janreimers@gmail.com>
2021-01-29 13:58:55 -0600

    we don't need iMPO::Combine

--- TensorNetworks/iMPO.C
+++ TensorNetworks/iMPO.C
@@ -5,14 +5,5 @@
 namespace TensorNetworks
 {
 
-void iMPO::Product(const iMPO* O2)
-{
-    int L=GetL();
-    assert(L==O2->GetL());
-    for (int ia=1;ia<=L;ia++)
-    {
-        GetSiteOperator(ia)->Product(O2->GetSiteOperator(ia));
-    }
-}
 
 }
