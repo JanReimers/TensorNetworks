7966895eb4e5f93e94f312ffff5f07b6f59d557b
Jan Reimers <janreimers@gmail.com>
2021-02-01 14:56:46 -0600

    iMPO compression/Canon should include all edge sites since the MPOs wrap around.

--- TensorNetworks/iMPO.C
+++ TensorNetworks/iMPO.C
@@ -4,6 +4,32 @@
 
 namespace TensorNetworks
 {
+double iMPO::Compress(CompressType ct, const SVCompressorR* compressor)
+{
+    double terror=0.0;
+    int L=GetL();
+    for (int ia=1;ia<=L;ia++)
+    {
+//        std::cout << "Site " << ia << ": ";
+        double err=GetSiteOperator(ia)->Compress(ct,DLeft ,compressor);
+        terror+=err*err;
+    }
+    for (int ia=L;ia>=1;ia--)
+    {
+//        std::cout << "Site " << ia << ": ";
+        double err=GetSiteOperator(ia)->Compress(ct,DRight ,compressor);
+        terror+=err*err;
+    }
+    return sqrt(terror)/(L-1); //Truncation error per site.
+}
 
+void iMPO::CanonicalForm()
+{
+    int L=GetL();
+    for (int ia=1;ia<=L;ia++)
+        GetSiteOperator(ia)->CanonicalForm(DLeft);
+    for (int ia=L;ia>=1;ia--)
+        GetSiteOperator(ia)->CanonicalForm(DRight);
+}
 
 }
