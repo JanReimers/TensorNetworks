2696cc7f9ad63917887f96d6ef95d87f230b8b96
Jan Reimers <janreimers@gmail.com>
2022-01-03 12:09:54 -0600

    Fail to get VUMPS to converge

--- TensorNetworksUnittests/iVUMPSTests.C
+++ TensorNetworksUnittests/iVUMPSTests.C
@@ -11,6 +11,7 @@ using std::setw;
 //using TensorNetworks::TriType;
 using TensorNetworks::Random;
 using TensorNetworks::Neel;
+using TensorNetworks::Constant;
 using TensorNetworks::DLeft;
 using TensorNetworks::DRight;
 using TensorNetworks::Gates;
@@ -31,7 +32,7 @@ class iVUMPSTests : public ::testing::Test
 public:
 
     iVUMPSTests()
-    : epsNorm(4e-11)
+    : epsNorm(1e-14)
     , epsOrth(1e-11)
     , itsFactory(TensorNetworks::Factory::GetFactory())
     , itsiH(0)
@@ -87,10 +88,12 @@ TEST_F(iVUMPSTests,TestNormQR_D2_L1)
     {
         Setup(UnitCell,S,D,J,hx,epsSVD,f);
         itsiMPS->InitializeWith(Random);
-        itsiMPS->NormalizeQR(DLeft);
+        itsiMPS->NormalizeQR(DLeft,epsNorm);
         EXPECT_EQ(itsiMPS->GetNormStatus(),"A");
-        itsiMPS->NormalizeQR(DRight);
+        itsiMPS->NormalizeQR(DRight,epsNorm);
         EXPECT_EQ(itsiMPS->GetNormStatus(),"B");
+        double etaG=itsiMPS->GetGaugeError(); //Check ||A(k)*G(k)-G(k-1)B(k)||
+        EXPECT_NEAR(etaG,0.0,epsNorm);
     }
 }
 
@@ -103,10 +106,12 @@ TEST_F(iVUMPSTests,TestNormQR_D6_L1)
     {
         Setup(UnitCell,S,D,J,hx,epsSVD,f);
         itsiMPS->InitializeWith(Random);
-        itsiMPS->NormalizeQR(DLeft);
+        itsiMPS->NormalizeQR(DLeft,epsNorm);
         EXPECT_EQ(itsiMPS->GetNormStatus(),"A");
-        itsiMPS->NormalizeQR(DRight);
+        itsiMPS->NormalizeQR(DRight,epsNorm);
         EXPECT_EQ(itsiMPS->GetNormStatus(),"B");
+        double etaG=itsiMPS->GetGaugeError(); //Check ||A(k)*G(k)-G(k-1)B(k)||
+        EXPECT_NEAR(etaG,0.0,epsNorm);
     }
 }
 
@@ -115,14 +120,16 @@ TEST_F(iVUMPSTests,TestNormQR_D6_L10)
     int UnitCell=10,D=6;
     double J=-1.0,hx=0.0,epsSVD=0.0;
     MPOForm f=RegularLower;
-    for (double S=0.5;S<=2.5;S+=0.5)
+    for (double S=0.5;S<=0.5;S+=0.5)
     {
         Setup(UnitCell,S,D,J,hx,epsSVD,f);
         itsiMPS->InitializeWith(Random);
-        itsiMPS->NormalizeQR(DLeft);
+        itsiMPS->NormalizeQR(DLeft,epsNorm);
         EXPECT_EQ(itsiMPS->GetNormStatus(),"AAAAAAAAAA");
-        itsiMPS->NormalizeQR(DRight);
+        itsiMPS->NormalizeQR(DRight,epsNorm);
         EXPECT_EQ(itsiMPS->GetNormStatus(),"BBBBBBBBBB");
+        double etaG=itsiMPS->GetGaugeError(); //Check ||A(k)*G(k)-G(k-1)B(k)||
+        EXPECT_NEAR(etaG,0.0,epsNorm);
     }
 }
 //
@@ -205,7 +212,7 @@ TEST_F(iVUMPSTests,TestFindFerroGS_S32_D8_L1_h04)
     double S=1.5,J=-1.0,hx=0.4,epsSVD=0.0,epsE=1e-12;
     MPOForm f=RegularLower;
     Setup(UnitCell,S,D,J,hx,epsSVD,f);
-    itsiMPS->InitializeWith(Random);
+    itsiMPS->InitializeWith(Constant);
     Epsilons eps(epsE);
     eps.itsDeltaLambdaEpsilon=5e-9;
 
@@ -221,29 +228,29 @@ TEST_F(iVUMPSTests,TestFindFerroGS_S32_D8_L1_h04)
 //
 TEST_F(iVUMPSTests,TestFindFerroGS_S12_D2_L2_h01)
 {
-    int UnitCell=2,D=2,maxIter=30;
+    int UnitCell=2,D=2,maxIter=10;
     double S=0.5,J=-1.0,hx=0.1,epsSVD=0.0,epsE=1e-14;
     MPOForm f=RegularLower;
     Setup(UnitCell,S,D,J,hx,epsSVD,f);
     itsiMPS->InitializeWith(Random);
     Epsilons eps(epsE);
-    eps.itsDeltaLambdaEpsilon=1e-8;
+    eps.itsDeltaLambdaEpsilon=1e-14;
 
     IterationSchedule is;
     is.Insert({maxIter,D,eps});
     double Eeigen=itsiMPS->FindVariationalGroundState(itsiH,is);
     double Eex   =itsiMPS->GetExpectation(itsiH);
     EXPECT_NEAR(Eeigen,Eex,2e-6);
-    EXPECT_NEAR(Eex,-0.25250795696355421,epsE);
+    EXPECT_NEAR(Eex,-0.25250799805,epsE);
 }
 
 TEST_F(iVUMPSTests,TestFindAFGS_S12_D2_L2_h01)
 {
     int UnitCell=2,D=2,maxIter=30;
-    double S=0.5,J=-1.0,hx=0.1,epsSVD=0.0,epsE=1e-14;
+    double S=0.5,J=1.0,hx=0.1,epsSVD=0.0,epsE=1e-14;
     MPOForm f=RegularLower;
     Setup(UnitCell,S,D,J,hx,epsSVD,f);
-    itsiMPS->InitializeWith(Random);
+    itsiMPS->InitializeWith(Constant);
     Epsilons eps(epsE);
     eps.itsDeltaLambdaEpsilon=1e-8;
 
